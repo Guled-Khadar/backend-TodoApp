af9a504d40a88327830e1150d3c7b5d8249d42b3
Guled <guletkadar@gmail.com>
2023-09-11 20:49:11 +0300

    Allowing to acces my db

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const router = require("./routes/todoRoutes")
 const app = express()
 require("dotenv").config()
@@ -9,10 +10,28 @@ const connectToMongo = require("./db/connection")
 //Port
 const PORT = process.env.PORT
 
+// sites that i allowed to fetch my data 
+
+const myListSites = "https://musical-conkies-2d4293.netlify.app/"
+const corsOption = {
+    origin : (origin , callback)=> {
+        if(myListSites.origin(origin)!== -1){
+            callback(null, true)
+        }else {
+            callback( error = (" am not allowes to acces these site "))
+        }
+
+        optionIsSuccessfull = 200
+
+
+    }
+}
+
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(router)
+app.use(cors())
 
 
 //running port 
